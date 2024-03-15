2787e637f5f61e5156bc69c711814736e15c6186
Simone Manca <simone.manca.98@gmail.com>
2024-03-15 11:02:08 +0100

    classiLibriAndRiviste

--- src/main/java/simonemanca/Application.java
+++ src/main/java/simonemanca/Application.java
@@ -1,8 +1,35 @@
 package simonemanca;
 
-public class Application {
+import simonemanca.catalogo.Libro;
+import simonemanca.catalogo.Rivista;
+import com.github.javafaker.Faker;
 
+import java.util.Locale;
+
+public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Faker faker = new Faker(new Locale("it"));
+
+        Libro fakeBook = new Libro(
+                faker.code().isbn13(),
+                faker.book().title(),
+                faker.number().numberBetween(1900, 2021),
+                faker.number().numberBetween(100, 1000),
+                faker.book().author(),
+                faker.book().genre()
+        );
+
+        Rivista.Periodicita periodicita = Rivista.Periodicita.values()[faker.number().numberBetween(0, Rivista.Periodicita.values().length)];
+        Rivista fakeMagazine = new Rivista(
+                faker.code().isbn13(),
+                faker.book().title(),
+                faker.number().numberBetween(1900, 2021),
+                faker.number().numberBetween(20, 100),
+                periodicita
+        );
+
+        System.out.println("Libro: " + fakeBook.getTitolo() + ", Autore: " + fakeBook.getAutore());
+        System.out.println("Rivista: " + fakeMagazine.getTitolo() + ", Periodicità: " + fakeMagazine.getPeriodicita());
     }
 }
+
---
+++ src/main/java/simonemanca/catalogo/Rivista.java
@@ -0,0 +1,20 @@
+package simonemanca.catalogo;
+
+public class Rivista extends CatalogoItem {
+    public enum Periodicita { SETTIMANALE, MENSILE, SEMESTRALE }
+    private Periodicita periodicita;
+
+    public Rivista(String isbn, String titolo, int annoPubblicazione, int numeroPagine, Periodicita periodicita) {
+        super(isbn, titolo, annoPubblicazione, numeroPagine);
+        this.periodicita = periodicita;
+    }
+
+    public Periodicita getPeriodicita() {
+        return periodicita;
+    }
+
+    public void setPeriodicita(Periodicita periodicita) {
+        this.periodicita = periodicita;
+    }
+}
+
